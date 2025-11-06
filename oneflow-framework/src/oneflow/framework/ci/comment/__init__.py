2321f5b6124ced80b030c13f1643e74eb3c70346
narramukhesh <mukheshnarra@gmail.com>
2025-11-06 13:09:30 +0000

    feat: (oneflow)(framework)(ISS-4) Added oneflow framework package which contains all data processing contract specification to execute the series of the data processing tasks

---
+++ oneflow-framework/src/oneflow/framework/ci/comment/__init__.py
@@ -0,0 +1,13 @@
+from oneflow.core.schemas import ParentEnum
+from oneflow.framework.ci.comment.gitlab import GitLabClient
+
+
+class CIPlatformType(ParentEnum):
+    """CICD Platform types supported"""
+
+    gitlab = "gitlab"
+
+
+def get_ci_client(ci_platform: CIPlatformType):
+    if ci_platform == CIPlatformType.gitlab:
+        return GitLabClient()
